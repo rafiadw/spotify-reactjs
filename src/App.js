654e246db7cfbb25d9a4286c74577e037fe27625
rafiadw <rafiadw@gmail.com>
2022-04-02 13:17:00 +0700

    try to fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-
-import './App.css';
+import "./App.css";
 // import Data from './data/data'
 // import Card from './components/cardSong/index';
-import Spotify from './components/spotify/index';
-
+import Spotify from "./pages/home/index";
 
 function App() {
   const url = `https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json`;
-  fetch(url).then(response => console.log(response.data))
+  fetch(url).then((response) => console.log(response.data));
 
   return (
     <div className="App">
-      <Spotify/>
-      
+      <Spotify />
     </div>
   );
 }
