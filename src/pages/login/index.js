56431a830aee6d4e2c38bd761bbf1f7601aa60b3
rafiadw <rafiadw@gmail.com>
2022-04-14 08:21:10 +0700

    test

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,36 +1,34 @@
 import login from "../../services/auth-login";
-//import style from "./style.module.css";
-import { Heading, Box, Text, Button, Container } from "@chakra-ui/react";
+import style from "./style.module.css";
+//import { Heading, Box, Text, Button, Container } from "@chakra-ui/react";
 
 function Login() {
   return (
-    // Example from paystack.com
-    <Container mt={300} centerContent>
-      <Box maxW="32rem">
-        <Heading mb={4} centerContent>
-          Hello, there!
-        </Heading>
-        <Text fontSize="xl">
-          Please, login. Before you using this web application
-        </Text>
-        <Button size="lg" colorScheme="green" mt="24px" onClick={() => login()}>
-          Login
-        </Button>
-      </Box>
-    </Container>
+    <div className={style.wrapper}>
+      <div className={style.content}>
+        <h1>Hello, there!</h1>
+        <p></p>
+      </div>
+      <div className={style.button}>
+        <button onClick={() => login()}>Login</button>
+      </div>
+    </div>
   );
 }
 
 export default Login;
 
-{
-  /* <div className={style.wrapper}>
-  <div className={style.content}>
-    <h1>Hello, there!</h1>
-    <p></p>
-  </div>
-  <div className={style.button}>
-    <button onClick={() => login()}>Login</button>
-  </div>
-</div>; */
-}
+// Example from paystack.com
+// <Container mt={300} centerContent>
+//   <Box maxW="32rem">
+//     <Heading mb={4} centerContent>
+//       Hello, there!
+//     </Heading>
+//     <Text fontSize="xl">
+//       Please, login. Before you using this web application
+//     </Text>
+//     <Button size="lg" colorScheme="green" mt="24px" onClick={() => login()}>
+//       Login
+//     </Button>
+//   </Box>
+// </Container>
