1c1e0e83d436757c1cf3faad838b0ee1ee774562
rafiadw <rafiadw@gmail.com>
2022-04-21 15:40:44 +0700

    change reidirect uri

--- src/services/auth-login.js
+++ src/services/auth-login.js
@@ -1,7 +1,6 @@
 const login = () => {
   const CLIENT_ID = process.env.REACT_APP_SPOTIFY_API;
-  const REDIRECT_URI =
-    "https://rafiadw-spotify-b721k3six-rafiadw.vercel.app/callback";
+  const REDIRECT_URI = "https://rafiadw-spotify.vercel.app/callback";
   const SCOPE = "playlist-modify-private";
   const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
   const AUTH_URL = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=token&show_dialog=true`;
